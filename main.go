7365ef4321ea433f199df175c18439f9f93079ce
Phạm Sơn Trà <1212432@fit.hcmus.edu.vn>
2016-05-27 21:58:34 +0700

    add 1212432.html and 1212439.html update main.go to handle these pages

--- main.go
+++ main.go
@@ -5,13 +5,44 @@ import (
     "html/template"
 )
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, nil)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "contact")
+}
+
+func personOneHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "1212432")
+}
+
+func personTwoHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "1212439")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+    t, err := template.ParseFiles(tmpl + ".html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    err = t.Execute(w, nil)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func main() {
-    http.HandleFunc("/index/", viewHandler)
+    http.HandleFunc("/index/", indexHandler)
+    http.HandleFunc("/about/", aboutHandler)
+    http.HandleFunc("/contact/", contactHandler)
+    http.HandleFunc("/1212432/", personOneHandler)
+    http.HandleFunc("/1212439/", personTwoHandler)
     fs := http.FileServer(http.Dir(""))
 	http.Handle("/css/", fs)
 	http.Handle("/images/", fs)
